98bf2f4ccde7158da988899e6ccf3a2e556dc5c2
nitine <nitin.spaced@gmail.com>
2025-01-02 20:56:09 +0530

    feat: setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,11 @@ import TeamMembersAndLeaveButton from "@/components/team-dashboard";
 import Winners from "@/components/winners";
 import type React from "react";
 import { auth } from "./(auth)/auth";
-
+import "./globals.css";
+import { Plus_Jakarta_Sans } from "next/font/google";
+const plus_jakarta_sans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 // till auth is implemeneted basic routing for now
-const isAdmin = false; // true --> admin
+const isAdmin = true; // true --> admin
 const detailsFilled = true;
 const teamJoined = true;
 const teamCheckedIn = true;
@@ -19,96 +21,110 @@ const noPendingRound = true;
 const disqualified = false;
 
 export default async function RootLayout({
-  children,
-  team,
-  admin,
-  landing,
+	children,
+	team,
+	admin,
+	landing,
 }: {
-  children: React.ReactNode;
-  team: React.ReactNode;
-  admin: React.ReactNode;
-  landing: React.ReactNode;
+	children: React.ReactNode;
+	team: React.ReactNode;
+	admin: React.ReactNode;
+	landing: React.ReactNode;
 }) {
-  const session = await auth();
-  if (!session?.user?.email) {
-    return (
-      <html lang="en">
-        <body>{landing}</body>
-      </html>
-    );
-  }
+	const session = await auth();
+	if (!session?.user?.email) {
+		return (
+			<html lang="en">
+				<body>{landing}</body>
+			</html>
+		);
+	}
 
-  if (isAdmin) {
-    return (
-      <html lang="en">
-        <body>{admin}</body>
-      </html>
-    );
-  }
+	if (isAdmin) {
+		return (
+			<html lang="en">
+				<body className={plus_jakarta_sans.className}>{admin}</body>
+			</html>
+		);
+	}
 
-  if (!detailsFilled) {
-    return (
-      <html lang="en">
-        <body><DetailsForm/></body>
-      </html>
-    );
-  }
-  if(!teamJoined) {
-    return (
-      <html lang="en">
-        <body>{team}</body>
-      </html>
-    );
-  }
+	if (!detailsFilled) {
+		return (
+			<html lang="en">
+				<body>
+					<DetailsForm />
+				</body>
+			</html>
+		);
+	}
+	if (!teamJoined) {
+		return (
+			<html lang="en">
+				<body>{team}</body>
+			</html>
+		);
+	}
 
-  if (!teamCheckedIn) {
-    return (
-      <html lang="en">
-        <body><TeamMembersAndLeaveButton/></body>
-      </html>
-    );
-  }
+	if (!teamCheckedIn) {
+		return (
+			<html lang="en">
+				<body>
+					<TeamMembersAndLeaveButton />
+				</body>
+			</html>
+		);
+	}
 
-  if (disqualified) {
-    return (
-      <html lang="en">
-        <body><Disqualified/></body>
-      </html>
-    );
-  }
-  if (roundIsActive) {
-    if (memberOfActiveRound) {
-      return (
-        <html lang="en">
-          <body>{children}</body>
-        </html>
-      );
-    }
-    return (
-      <html lang="en">
-        <body><EliminationScreen/></body>
-      </html>
-    );
-  }
+	if (disqualified) {
+		return (
+			<html lang="en">
+				<body>
+					<Disqualified />
+				</body>
+			</html>
+		);
+	}
+	if (roundIsActive) {
+		if (memberOfActiveRound) {
+			return (
+				<html lang="en">
+					<body>{children}</body>
+				</html>
+			);
+		}
+		return (
+			<html lang="en">
+				<body>
+					<EliminationScreen />
+				</body>
+			</html>
+		);
+	}
 
-  if (noPendingRound) {
-    if (winnersAnnounced) {
-      return (
-        <html lang="en">
-          <body><Winners /></body>
-        </html>
-      );
-    }
-    return (
-      <html lang="en">
-        <body><CountdownTimer/></body>
-      </html>
-    );
-  }
+	if (noPendingRound) {
+		if (winnersAnnounced) {
+			return (
+				<html lang="en">
+					<body>
+						<Winners />
+					</body>
+				</html>
+			);
+		}
+		return (
+			<html lang="en">
+				<body>
+					<CountdownTimer />
+				</body>
+			</html>
+		);
+	}
 
-  return (
-    <html lang="en">
-      <body><CountdownTimer/></body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<CountdownTimer />
+			</body>
+		</html>
+	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,25 @@
 import type { Config } from "tailwindcss";
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+const config: Config = {
+	darkMode: ["class"],
+
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+			boxShadow: {
+				"sharp-sm": "0 2px 0 0 rgba(0,0,0,0.25)",
+				"sharp-lg": "0 4px 0 0 rgba(0,0,0,0.25)",
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;

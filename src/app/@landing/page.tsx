152b8dcf0356802117609cba5a5409cdd60f6888
Abhi9 <abhisingh2344s@gmail.com>
2025-01-13 02:00:27 +0530

    Feat: "Prize Pool page"

--- src/app/@landing/page.tsx
+++ src/app/@landing/page.tsx
@@ -4,11 +4,14 @@ import LandingNavbar from '@/components/landing/landing-navbar';
 import HowItWorks from '@/components/landing/howitworks';
 import PrizePool from '@/components/landing/prizepool';
 
+
 const Page: React.FC = () => {
   return (
-    <div className='bg-black min-h-screen text-white'>
-    
-      <PrizePool />
+    <div className='bg-black min-h-screen'>
+      <LandingNavbar />
+      <LandOne />
+      <HowItWorks />
+     <PrizePool />
     </div>
   );
 };

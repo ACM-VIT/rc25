a267af7c8fe87c4c2b473bbe554808f00ad826aa
Mahendra785 <Mahendra785>
2025-01-20 22:57:44 +0530

    feat: added new folder for new design

---
+++ src/components/landing/new-pc/landing.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, World! All the best for the next 24 hrs</h1>
+    </div>
+  );
+};
+
+export default Landing;

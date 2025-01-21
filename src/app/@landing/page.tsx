16f54efec213d2a5ca35bd110ab7f0ffd8ea13e3
SarupaVL <lakshmisarupa@gmail.com>
2025-01-22 02:09:37 +0530

    timeline

--- src/app/@landing/page.tsx
+++ src/app/@landing/page.tsx
@@ -8,37 +8,12 @@ import FaqRegister from "@/components/landing/faqregister";
 import SponsorsSection from "@/components/landing/sponsors";
 import Footer from "@/components/landing/footer";
 import Landing from "@/components/landing/new-pc/landing";
-const Page: React.FC = () => {
-  const timelineData = [
-    {
-      title: "8:30 am",
-      content: <p className="text-neutral-300">Report At the Venue</p>,
-    },
-    {
-      title: "9:30 am",
-      content: <p className="text-neutral-300">Registrations Close</p>,
-    },
-    {
-      title: "10:00 am",
-      content: <p className="text-neutral-300">Round 1</p>,
-    },
-    {
-      title: "1:00 pm",
-      content: <p className="text-neutral-300">Lunch Break</p>,
-    },
-    {
-      title: "2:00 pm",
-      content: <p className="text-neutral-300">Round 2</p>,
-    },
-    {
-      title: "5:00 pm",
-      content: <p className="text-neutral-300">Closing Ceremony</p>,
-    },
-  ];
+import TimeLine from "@/components/landing/new-pc/timeline";
 
+const Page: React.FC = () => {
   return (
     <div className="overflow-x-hidden bg-black min-h-screen text-white">
-      <Landing />
+      <TimeLine />
     </div>
   );
 };

4e8182a01946f2b7263f6035c6bd839b0ab7c6e6
Alan-J-Bibins <alanjbibins@gmail.com>
2025-01-22 00:33:19 +0530

    feat: Floating Navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import {nextui} from '@nextui-org/theme';
 import type { Config } from "tailwindcss";
+import tailwindcssMotion from "tailwindcss-motion";
 
 const config: Config = {
 	darkMode: ["class"],
@@ -80,11 +81,8 @@ const config: Config = {
 				fadeIn: "fadeIn 1.5s ease-in-out",
 			},
 		},
-		plugins: [
-			require('tailwindcss-motion'), 
-		],
 	},
 	
-  plugins: [nextui()],
+  plugins: [nextui(), tailwindcssMotion],
 };
 export default config;

2c94b179f03b07b151990ebca06606df4d2aba70
Van5sh <vansh05dhir@gmail.com>
2025-02-05 03:59:55 +0530

    live leaderboard added

---
+++ src/app/@admin/liveleaderboard/page.tsx
@@ -0,0 +1,9 @@
+import LiveLeaderboard from "@/app/@admin/components/liveleaderboard";
+
+export default function AdminLeaderboard(){
+    return(
+        <div>
+            <LiveLeaderboard/>
+        </div>
+    )
+}

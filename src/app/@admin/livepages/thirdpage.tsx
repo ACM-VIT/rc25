f77efee7bc5d117d7df06a5d661503ded49d2505
Van5sh <vansh05dhir@gmail.com>
2025-02-05 05:05:52 +0530

    fixes

--- src/app/@admin/livepages/thirdpage.tsx
+++ src/app/@admin/livepages/thirdpage.tsx
@@ -7,7 +7,7 @@ export default function SecondPage(){
             </div>            
             <div className="flex flex-col justify-center items-center bg-[#606060]/80 p-6 rounded-lg h-[90vh] text-center">
                 <p className="text-3xl">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.
                 </p>
             </div>
         </div>
--- src/components/countdownpage.tsx
+++ src/components/countdownpage.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import React, {  useState } from "react";
+import React from "react";
 import Image from "next/image";
 import bg from "@/app/assets/backgroundone.svg";
 import earthone from "@/app/assets/rounded_earth.svg";
-import { Orbitron } from "next/font/google";
 import CountdownTimer from "@/components/countdown-timer";
 
 

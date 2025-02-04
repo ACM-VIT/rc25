681859f00973b2a267b136573760e6105c5cc3c6
Abhi9 <abhisingh2344s@gmail.com>
2025-02-04 17:10:26 +0530

    Feat:Timer component and countdown page

--- src/components/FloatingDock.tsx
+++ src/components/FloatingDock.tsx
@@ -1,35 +1,37 @@
 import Image from "next/image";
 import Link from "next/link";
 
-function FloatingDockItems({ href, src, tooltip, className }: { href: string, src: string,tooltip:string, className?: string }) {
+function FloatingDockItems({ href, src, className, tooltip }: { href: string, src: string, className?: string, tooltip: string }) {
     return (
-        <div className="relative group">
+        <div className="relative flex justify-center items-center group">
             <Link
                 href={href}
-                className={`bg-weirdPurple/40 rounded-full transition-transform duration-300 ease-in-out hover:scale-125 hover:-translate-y-1 hover:text-accent ${className}`}
+                className={`bg-weirdPurple/40 p-2 rounded-full transition-transform duration-300 ease-in-out 
+                hover:scale-125 hover:-translate-y-1 hover:text-accent ${className}`}
             >
                 <Image
                     src={src}
                     alt={href}
                     width={30}
                     height={30}
-                    className="transition-transform duration-300 ease-in-out"
+                    className="transition-transform duration-50 ease-in-out"
                 />
-                <span
-                    className="absolute left-1/2 -translate-x-1/2 bottom-full mb-2 px-2 py-1 text-xs text-white bg-black rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap">
-                {tooltip}
-            </span>
             </Link>
+            {/* Tooltip */}
+            <div className="absolute bottom-14 left-1/2 -translate-x-1/2 px-2 py-1 text-xs text-white 
+                bg-black/80 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap pointer-events-none">
+                {tooltip}
+            </div>
         </div>
-            );
-        }
+    );
+}
 
-            export default function FloatingDock() {
-            return (
-            <div className="bg-secondary/60 text-text flex shadow-lg justify-between backdrop-blur-3xl
-                w-[280px] px-6 py-2 rounded-xl fixed bottom-2 left-1/2 transform -translate-x-1/2
-                z-50 shadow-black/20 hover:shadow-xl hover:-translate-y-2 hover:w-[320px]
-                hover:scale-105 transition-all duration-500 ease-in-out gap-x-4">
+export default function FloatingDock() {
+    return (
+        <div className="bg-secondary/60 text-text flex shadow-lg justify-between backdrop-blur-3xl
+            w-[280px] px-6 py-2 rounded-xl fixed bottom-2 left-1/2 transform -translate-x-1/2
+            z-50 shadow-black/20 hover:shadow-xl hover:-translate-y-2 hover:w-[320px]
+            hover:scale-105 transition-all duration-200 ease-in-out gap-x-4">
             <FloatingDockItems
                 href="/"
                 tooltip="Home"
@@ -60,6 +62,6 @@ function FloatingDockItems({ href, src, tooltip, className }: { href: string, sr
                 className="motion-preset-slide-up-md motion-delay-[700ms]"
                 src="/floatingDock/vader.svg"
             />
-            </div>
-            );
-        }
+        </div>
+    );
+}

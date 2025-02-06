34d78102a53bfc75dd7124b29cfbb5cbb5d56179
aditya singh <adityasingh789m@gmail.com>
2025-02-07 03:31:29 +0530

    responsiveness and font fixes

--- src/components/disqualifed.tsx
+++ src/components/disqualifed.tsx
@@ -7,8 +7,8 @@ import image4 from "../../public/image4.svg";
 const Disqualified = () => {
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-[#0d0b17] text-white text-center overflow-hidden">
-      <Image src={image4} alt="Centered SVG" className="w-1/3 h-auto" />
-      <p className="text-4xl font-bold mt-6"  style={{ fontFamily: 'Death Star, sans-serif' }}>
+      <Image src={image4} alt="Centered SVG" className="w-1/2 h-auto" />
+      <p className="text-3xl lg:text-5xl font-bold mt-6 deathstar tracking-wider"   >
       Expelled, you have been.
       </p>
      
--- src/components/elimination-screen.tsx
+++ src/components/elimination-screen.tsx
@@ -7,8 +7,8 @@ import ButThe from "../../public/butthe.svg";
 const MemeDisplay: React.FC = () => {
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-[#0d0b17] text-white text-center overflow-hidden">
-      <Image src={ButThe} alt="Centered SVG" className="w-1/3 h-auto" />
-      <p className="text-4xl font-bold mt-6"  style={{ fontFamily: 'Death Star, sans-serif' }}>
+      <Image src={ButThe} alt="Centered SVG" className="w-1/3 lg:1/3 h-auto" />
+      <p className="text-3xl lg:text-5xl font-bold mt-6 deathstar tracking-wider" >
         NEXT TIME, LUCK BETTER IT MAY BE, HMMM?
       </p>
        

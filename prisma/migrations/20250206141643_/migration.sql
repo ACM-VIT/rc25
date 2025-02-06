888c1ee9eaa0fba268157c137d23e9b2ccab0d3f
manan04shah <manan04shah@gmail.com>
2025-02-06 19:57:10 +0530

    fix: venue removed

---
+++ prisma/migrations/20250206141643_/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `venue` on the `Team` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Team" DROP COLUMN "venue";
+
+-- DropEnum
+DROP TYPE "Venue";

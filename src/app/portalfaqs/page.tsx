b4f48798e4f956f5f940b1fe31036bb1672ff3e6
Supratim Ghose <supratim7ghose@gmail.com>
2025-02-07 03:47:23 +0530

    feat: epty

--- src/app/portalfaqs/page.tsx
+++ src/app/portalfaqs/page.tsx
@@ -3,28 +3,30 @@ import PortalFaqs from "./portal-faqs";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "FAQ",
-  description:
-    "Frequently asked questions about the Reverse Coding competition",
-  openGraph: {
     title: "FAQ",
-    description: "Find answers to common questions about Reverse Coding",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-  },
-  icons: {
-    icon: "/favicon.ico",
-  },
+    description:
+        "Frequently asked questions about the Reverse Coding competition",
+    openGraph: {
+        title: "FAQ",
+        description: "Find answers to common questions about Reverse Coding",
+        type: "website",
+    },
+    robots: {
+        index: true,
+        follow: true,
+    },
+    icons: {
+        icon: "/favicon.ico",
+    },
 };
 
 const Page = () => {
-  return <>
-        <PortalFaqs />
-        <FloatingDock />
-    </>
+    return (
+        <>
+            <PortalFaqs />
+            <FloatingDock />
+        </>
+    );
 };
 
 export default Page;

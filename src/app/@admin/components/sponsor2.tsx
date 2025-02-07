7ceeb11d7db5d4ca5da10ab638c5a4a2ad1584cd
theg <52027622+theg1239@users.noreply.github.com>
2025-02-07 06:37:14 +0530

    escaped apos

--- src/app/@admin/components/sponsor2.tsx
+++ src/app/@admin/components/sponsor2.tsx
@@ -28,7 +28,7 @@ const Sponsor2: React.FC = () => {
 
         <div className="flex flex-col justify-center items-center bg-[#1a1a2e]/70 p-6 rounded-lg h-[90vh] text-center">
           <p className="text-4xl text-white font-['Orbitron'] leading-relaxed tracking-wide">
-            Judge0, a leading provider of robust, scalable, open-source online code execution systems, was founded in 2016. With the motto "Where Code Happens," Judge0 promotes unparalleled innovation by delivering efficient, reliable and highly scalable online code execution solutions for educational, competitive coding and various other platforms.
+            Judge0, a leading provider of robust, scalable, open-source online code execution systems, was founded in 2016. With the motto &quot;Where Code Happens,&quot; Judge0 promotes unparalleled innovation by delivering efficient, reliable and highly scalable online code execution solutions for educational, competitive coding and various other platforms.
           </p>
         </div>
       </div>
